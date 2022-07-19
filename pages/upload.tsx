57f9119c928607ea50de450f1b20e096d26c4853
Anas Youssef <anasyoussef649@gmail.com>
2022-07-19 22:25:36 +0200

    first

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -87,8 +87,8 @@ const Upload = () => {
   };
 
   return (
-    <div className="flex w-full h-full absolute left-0 top-[60px] lg:top-[70px] mb-10 pt-10 lg:pt-20 bg-[#F8F8F8] justify-center">
-      <div className=" bg-white rounded-lg xl:h-[80vh] flex gap-6 flex-wrap justify-center items-center p-14 pt-6">
+    <div className="flex w-full h-full absolute left-0 top-[60px] lg:top-[60px] mb-10 pt-10 lg:pt-20 bg-[#F8F8F8] justify-center">
+      <div className=" bg-white rounded-lg xl:h-[90vh] flex gap-6 flex-wrap justify-center items-center p-14 pt-6">
         <div>
           <div>
             <p className="text-2xl font-bold animate-pulse">Upload Video</p>
