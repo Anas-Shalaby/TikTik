5e1407710c404fe27b17d573c611bfb27556ab05
Anas Youssef <anasyoussef649@gmail.com>
2022-07-19 20:51:46 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  // swcMinify: true,
+  images: {
+    domains: [
+      "t4.ftcdn.net",
+      "lh3.googleusercontent.com",
+      "pbs.twimg.com",
+      "unsplash.com",
+      "images.unsplash.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
